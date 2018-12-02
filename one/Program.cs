5b656150b4d0750f03f3c77c49c97b1542884ec6
Daan Roeterink <dragon321@gmail.com>
2018-12-02 12:49:13 +0100

    First day

---
+++ one/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace one
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Second();
+        }
+
+        public void First()
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            int start = 0;
+
+            foreach (var line in lines)
+            {
+                int change = int.Parse(line.Replace("+", string.Empty));
+
+                start += change;
+            }
+
+            Console.WriteLine($"First part:{start}");
+
+            Console.ReadLine();
+        }
+
+        public void Second()
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var list = new List<int>();
+
+            var start = 0;
+            
+            var found = false;
+
+            while (!found)
+            {
+                foreach (var line in lines)
+                {
+                    int change = int.Parse(line.Replace("+", string.Empty));
+
+                    var result = start + change;
+
+                    if (list.Contains(result))
+                    {
+                        found = true;
+                        Console.WriteLine(result);
+                        break;
+                    }
+                    else
+                    {
+                        list.Add(result);
+                        start = result;
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
